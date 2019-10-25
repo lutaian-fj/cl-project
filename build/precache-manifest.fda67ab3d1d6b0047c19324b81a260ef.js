62d4755516593c355c0118bbdfabc56eb543715d
卢泰桉 <890425@nd.com>
2019-10-25 18:38:08 +0800

    打包

---
+++ build/precache-manifest.fda67ab3d1d6b0047c19324b81a260ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e70c2fe6395852afa9788f51529519",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49147c2ed99896d2beba",
+    "url": "./static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7a5e435a97cd1f1b61ae",
+    "url": "./static/js/2.4a3d53c3.chunk.js"
+  },
+  {
+    "revision": "49147c2ed99896d2beba",
+    "url": "./static/js/main.1cc90907.chunk.js"
+  },
+  {
+    "revision": "49b492cbe57df9f15772",
+    "url": "./static/js/runtime-main.d529392b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
